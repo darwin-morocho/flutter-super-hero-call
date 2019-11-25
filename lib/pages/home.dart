d00ffae5977809240892e9738bd685d810787178
Darwin Morocho <darwin@MBP-de-Darwin.local.tld>
2019-11-24 20:59:17 -0500

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:super_hero_call/models/super_hero.dart';
 import '../utils/socket_client.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,10 +14,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   SocketClient _socketClient = new SocketClient();
 
+  Map<String, SuperHero> _superHeroes = Map();
+
   @override
   void initState() {
     super.initState();
     _socketClient.connect();
+    _socketClient.onConnected = (data) {
+      print(data.length);
+      setState(() {
+        _superHeroes = data;
+      });
+    };
   }
 
   @override
@@ -25,6 +36,54 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff263238),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pick your Super Hero",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  letterSpacing: 1,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Choose one to enter to Super Hero Chat",
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  letterSpacing: 1,
+                  fontWeight: FontWeight.w300),
+            ),
+            Container(
+              height: 1,
+              width: 100,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              color: Colors.white,
+            ),
+            Wrap(
+              children: _superHeroes.values.map((superHero) {
+                return CupertinoButton(
+                  onPressed: () {},
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: CachedNetworkImage(
+                        imageUrl: superHero.avatar,
+                        width: 100,
+                        height: 100,
+                      )),
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/socket_client.dart
+++ lib/utils/socket_client.dart
@@ -1,16 +1,39 @@
+import 'dart:convert';
+
 import 'package:adhara_socket_io/adhara_socket_io.dart';
+import 'package:super_hero_call/models/super_hero.dart';
+
+typedef OnConnected(Map<String, SuperHero> data);
 
 class SocketClient {
   final _manager = SocketIOManager();
   SocketIO _socket;
+  OnConnected onConnected;
 
   Future<void> connect() async {
     final options =
         SocketOptions("http://192.168.1.35:5000", enableLogging: true);
     _socket = await _manager.createInstance(options);
 
-    _socket.on('on-connected', (superHeroes) {
-      print("heroes ${superHeroes.toString()}");
+    _socket.on('on-connected', (dynamic data) {
+      try {
+        final Map<String, SuperHero> mapData = Map();
+
+        final tmp = new Map<String, dynamic>.from(jsonDecode(jsonEncode(data)));
+
+        tmp.forEach((key, value) {
+          print("value $value");
+          final superHero = SuperHero.fromJson(value);
+          mapData[key] = superHero;
+        });
+
+        print("send data ${mapData.length}");
+        if (onConnected != null) {
+          onConnected(mapData);
+        }
+      } catch (e) {
+        print(e);
+      }
     });
 
     _socket.onConnectError((error) {
